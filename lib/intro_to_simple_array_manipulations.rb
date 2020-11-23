40ad2392b4416fed035273af800d63b535432a88
James DeSousa <james.desousa45@gmail.com>
2020-11-23 21:58:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,10 @@ def using_flatten(array)
 end 
 
 def using_delete(array, string) 
-  
+  array.delete(string) 
+end 
+
+def using_delete_at (array, integer) 
+  array.delete_at(integer) 
+end 
   
